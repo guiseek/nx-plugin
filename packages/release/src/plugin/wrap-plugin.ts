9a15179333327e42b8979bcbffc79c3a5adc6610
Gui Seek <guiseek@gmail.com>
2022-08-02 14:08:41 -0300

    feat: release plugin

--- packages/release/src/plugin/wrap-plugin.ts
+++ packages/release/src/plugin/wrap-plugin.ts
@@ -25,10 +25,10 @@ export const wrapPlugin = (plugin: PluginFunction) => {
 
     if (!project) {
       logger.log(
-        "Skipping filtering of commits; no 'project' in configuration..."
+        "Bypassing commit filtering; no 'project' in configuration..."
       );
     } else {
-      logger.log(`Filtering commits to those affecting ${project}...`);
+      logger.log(`Filter commits to those that affect the project: ${project}...`);
 
       const paths = await getProjectChangePaths(project);
       logger.log(`Resolved to paths: ${paths.join(', ')}...`);
